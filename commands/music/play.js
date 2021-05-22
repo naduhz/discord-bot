8a125e1d96308af84076c6d78e158f97e809237f
jordan <jleejx2000@gmail.com>
2021-05-22 21:49:14 +0800

    updated packages, logic for play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -5,7 +5,6 @@ module.exports = {
     description: "Start playing from the queue.",
     
     async execute(message, args) {
-        
         // Fetch globalqueue and serverqueue
         const globalQueue = message.client.queue;
         const serverQueue = globalQueue.get(message.guild.id);
@@ -26,47 +25,71 @@ module.exports = {
             )
         };
         
-        // Join user's voice channel
-        const connection = voiceChannel.join();
-        if (connection) {
-            // Get song info from ytdl
-            try {
-                const songInfo = await ytdl.getInfo(args);
-                const song = {
+        // Get song info from ytdl
+        const songInfo = await ytdl.getInfo(args);
+        const song = {
                     title : songInfo.videoDetails.title,
                     url: songInfo.videoDetails.video_url
-            }} catch (error) {
-                console.log(error);
-                return message.channel.send(
-                    'I need a song to play!'
-                );
-            };
-
-            // Check for an existing server queue
-            if (!serverQueue) {
-                // Instantiate a server queue
-                const queueConstruct = {
-                    textChannel: message.channel,
-                    voiceChannel: voiceChannel,
-                    connection: null,
-                    songs: [],
-                    volume: 5,
-                    playing: true
                 };
-                
-                // Set the server queue into the global queue
-                globalQueue.set(message.guild.id, queueConstruct);
-                
-                // Push songs 
-                queueConstruct.songs.push(song);
-                console.log(queueConstruct.songs);
-                return message.channel.send(`${song.title} has been added to the queue!`)
-            } else {
-                serverQueue.songs.push(song);
-                console.log(serverQueue.songs);
-                return message.channel.send(`${song.title} has been added to the queue!`)
+        
+        
+
+        // Check for an existing server queue
+        if (!serverQueue) {
+            // Instantiate a server queue
+            const queueConstruct = {
+                textChannel: message.channel,
+                voiceChannel: voiceChannel,
+                connection: null,
+                songs: [],
+                volume: 5,
+                playing: true
             };
             
-        };
+            // Set the server queue into the global queue
+            globalQueue.set(message.guild.id, queueConstruct);
+            
+            // Push songs 
+            queueConstruct.songs.push(song);
+            console.log(queueConstruct.songs);
+            message.channel.send(`"${song.title}" has been added to the queue!`)
+            
+            // Join user's voice channel
+            try {
+                const connection = await voiceChannel.join();
+                queueConstruct.connection = connection;
+                // Play song
+                play(message.guild, queueConstruct.songs[0])
+            } catch (error) {
+                console.log(error);
+                globalQueue.delete(message.guild.id);
+                return message.channel.send(error);
+            }
+        } else {
+            serverQueue.songs.push(song);
+            console.log(serverQueue.songs);
+            return message.channel.send(`"${song.title}" has been added to the queue!`)
+        }
+
+        // Play function
+        function play(guild, song) {
+            // If serverQueue is empty, leave the voice channel and delete the queue.
+            const serverQueue = globalQueue.get(guild.id);
+            if (!song) {
+                message.channel.send('There are no songs left in the queue!')
+                serverQueue.voiceChannel.leave();
+                globalQueue.delete(guild.id);
+                return;
+            }
+
+            // Song dispatcher
+            const dispatcher = serverQueue.connection.play(ytdl(song.url)).on("finish", () => {
+                serverQueue.songs.shift();
+                play(guild, serverQueue.songs[0]);
+            }).on("error", error => console.error(error));
+            dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+            serverQueue.textChannel.send(`Now playing: "${song.title}"`);
+          }
+
+        }
     }
-}
--- commands/music/stop.js
+++ commands/music/stop.js
@@ -7,40 +7,28 @@ module.exports = {
         const serverQueue = globalQueue.get(message.guild.id);
 
         // Check that user is in the voice channel
-        const voiceChannel = message.member.voice.channel
-        if (!voiceChannel) {
+        const userVoiceChannel = message.member.voice.channel
+        if (!userVoiceChannel) {
             return message.channel.send(
                 'You have to be in a voice channel for me to stop the music!'
             );
         };
 
-        // Check that bot is in a voice channel
-        const botVoiceStatus = message.guild.voice.connection
-        if (!botVoiceStatus) {
-            return message.channel.send(
-                'I\'m not in a voice channel!'
-            );
-        }
-
-        // Match voice channel IDs
-        if (voiceChannel !== botVoiceStatus.channel) {
+        // Check that bot is in the voice channel
+        if (message.guild.voice == undefined || userVoiceChannel !== message.guild.voice.channel) {
             return message.channel.send(
                 'I\'m not in your voice channel!'
                 );
         } else {
-            // Disconnect from voice channel
-            message.channel.send('Bye bye!');
-            botVoiceStatus.disconnect();
-
             // Check for song in queue
             if (!serverQueue) {
-                return message.channel.send(
-                    'There is no song for me to stop!'
-                    );
+                message.channel.send('There is no song for me to stop, bye bye!');
+                message.guild.voice.setChannel(null);
+                return;
             }
-
             // Remove songs from queue, stop connecting to voice channel
             serverQueue.songs = [];
+            message.channel.send('Bye bye!');
             serverQueue.connection.dispatcher.end();
         }
     }
