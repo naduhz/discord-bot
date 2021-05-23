35338b782761911bfb419962586fd388107663a8
jordan <jleejx2000@gmail.com>
2021-05-23 21:56:47 +0800

    basic queue functionalities

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -1,7 +1,22 @@
 module.exports = {
     name: "queue",
     description: "Displays the queue.",
-    execute() {
+    async execute(message, args) {
+        // Fetch globalqueue and serverqueue
+        const globalQueue = message.client.queue;
+        const serverQueue = globalQueue.get(message.guild.id);
+
+        // CHeck if serverQueue
+        if (!serverQueue) {
+            return message.channel.send("There are are no songs queued at the moment!")
+        };
+
+        // Send a message with the queue
+        return message.channel.send(`Music Queue: \[${serverQueue.songs.length} track\(s\)\]`);
+        // TODO: in an embed
+            // TODO: that tracks the time remaining of current track
+            // TODO: displays track order by index, and length of each track
+            // TODO: and remove option
         
     }
 }
