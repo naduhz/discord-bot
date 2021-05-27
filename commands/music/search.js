5e94f192b6cecb6174ff7f54edf6ae9cfc92a78d
jordan <jleejx2000@gmail.com>
2021-05-27 15:41:52 +0800

    beautified

--- commands/music/search.js
+++ commands/music/search.js
@@ -7,12 +7,20 @@ module.exports = {
     
     async execute(message, args) {
         const searchString = args.join(' ');
-        const searchResults = await ytsr(searchString, {limit: 10});
-        console.log(searchResults.items);
+        
+        const filters = await ytsr.getFilters(searchString);
+        const filterVideo = filters.get('Type').get('Video');
+        const searchResults = await ytsr(filterVideo.url, {limit: 10});
 
         let desc = '';
-        for (i = 0; i < searchResults.items.length; i++)
-            desc += `${i+1} [${searchResults.items[i].title.substring(0, 50)}](${searchResults.items[i].url})\n`
+        for (i = 0; i < searchResults.items.length; i++) {
+            if (searchResults.items[i].title.length > 40) {
+                desc += `${i+1} [${searchResults.items[i].title.substring(0, 37)}...](${searchResults.items[i].url})\n`
+            } else {
+                desc += `${i+1} [${searchResults.items[i].title.substring(0, 40)}](${searchResults.items[i].url})\n`
+            }
+        };
+
         const embed = new Discord.MessageEmbed()
                 // Descriptors
                 .setTitle(`Results for "${searchString}":`)
@@ -21,5 +29,7 @@ module.exports = {
                 .setThumbnail(searchResults.items[0].bestThumbnail.url)
         
         message.channel.send(embed);
+
+        // TODO: Play from search
     }
 }
