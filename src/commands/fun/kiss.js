fd4419bde6d6202aac2551986665dcf5f48534d7
jordan <jleejx2000@gmail.com>
2021-06-13 20:36:41 +0800

    fixed kiss

--- src/commands/fun/kiss.js
+++ src/commands/fun/kiss.js
@@ -10,7 +10,7 @@ module.exports = {
   usage: `\`${prefix}kiss\``,
 
   async execute(message, args) {
-    const searchParameters = new SearchParameter("kiss", "anime", "gif");
+    const searchParameters = new SearchParameter("kiss", "anime", "anigif");
     const results = await imgurSearchRequests(searchParameters);
 
     const randomAlbum = results[Math.floor(Math.random() * results.length)];
@@ -18,9 +18,10 @@ module.exports = {
     try {
       if (!randomAlbum.hasOwnProperty("images")) {
         const embed = imageEmbed(randomAlbum.link);
-        embed.description = `${
-          message.author
-        } kisses ${message.mentions.members.first()}`;
+        embed.description =
+          message.mentions.members.size == 0
+            ? `${message.author} kisses`
+            : `${message.author} kisses ${message.mentions.members.first()}`;
         return message.channel.send(embed);
       } else {
         const randomImageLink =
@@ -28,9 +29,10 @@ module.exports = {
             Math.floor(Math.random() * randomAlbum.images.length)
           ].link;
         const embed = imageEmbed(randomImageLink);
-        embed.description = `${
-          message.author
-        } kisses ${message.mentions.members.first()}`;
+        embed.description =
+          message.mentions.members.size == 0
+            ? `${message.author} kisses`
+            : `${message.author} kisses ${message.mentions.members.first()}`;
         return message.channel.send(embed);
       }
     } catch (error) {
