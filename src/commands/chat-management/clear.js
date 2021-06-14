57a2555c8b9f74b44c5308fe7e57ca3f83b4e366
jordan <jleejx2000@gmail.com>
2021-06-14 18:10:33 +0800

    chat clear completed

--- src/commands/chat-management/clear.js
+++ src/commands/chat-management/clear.js
@@ -3,22 +3,79 @@ const prefix = process.env.prefix;
 module.exports = {
   name: "clear",
   category: "chat management",
-  description: "Clears a specified amount of messages.",
+  description: "Clears up to 100 messages at a time.",
   usage: `\`${prefix}clear [user] <amount>\``,
 
   async execute(message, args) {
     if (message.mentions.members.size > 0) {
-      const mentions = message.mentions.members.first();
+      const mentionedUserID = message.mentions.members.first().user.id;
       const filteredArgs = args.filter((argument) => {
         return !argument.startsWith("<@");
       });
-      const fetchedMessages = await message.channel.messages.fetch({
-        limit: filteredArgs[0],
-      });
+
+      if (filteredArgs.length > 0) {
+        const fetchedMessages = Array.from(
+          await message.channel.messages.fetch({
+            limit: 100,
+          })
+        );
+        const filteredMessages = fetchedMessages
+          .filter((message) => {
+            return message[1].author.id == mentionedUserID;
+          })
+          .map((message) => {
+            return message[1];
+          });
+
+        message.channel
+          .bulkDelete(filteredMessages.slice(0, +filteredArgs[0]))
+          .then((messages) => {
+            if (messages.size < +filteredArgs[0]) {
+              messages.size > 1
+                ? message.channel
+                    .send(
+                      `I could only find and delete ${messages.size} messages!`
+                    )
+                    .then((message) => message.delete({ timeout: 3000 }))
+                : message.channel
+                    .send(
+                      `I could only find and delete ${messages.size} message!`
+                    )
+                    .then((message) => message.delete({ timeout: 3000 }));
+            } else {
+              messages.size > 1
+                ? message.channel
+                    .send(`Deleted ${messages.size} messages!`)
+                    .then((message) => message.delete({ timeout: 3000 }))
+                : message.channel
+                    .send(`Deleted ${messages.size} message!`)
+                    .then((message) => message.delete({ timeout: 3000 }));
+            }
+          })
+          .catch(console.error);
+      } else {
+        message.channel.send("I need a number of messages to delete!");
+      }
     } else {
-      const fetchedMessages = await message.channel.messages.fetch({
-        limit: args[0],
-      });
+      if (args.length > 0) {
+        const fetchedMessages = await message.channel.messages.fetch({
+          limit: args[0],
+        });
+        message.channel
+          .bulkDelete(fetchedMessages)
+          .then((messages) => {
+            messages.size > 1
+              ? message.channel
+                  .send(`Deleted ${messages.size} messages!`)
+                  .then((message) => message.delete({ timeout: 3000 }))
+              : message.channel
+                  .send(`Deleted ${messages.size} message!`)
+                  .then((message) => message.delete({ timeout: 3000 }));
+          })
+          .catch(console.error);
+      } else {
+        message.channel.send("I need a number of messages to delete!");
+      }
     }
   },
 };
